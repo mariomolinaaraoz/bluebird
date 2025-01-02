75a2c9f4a8b21b9eeffc8324c690d320bb54d486
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2025-01-02 14:49:23 -0300

    first commit

--- app/auth-button-client.tsx
+++ app/auth-button-client.tsx
@@ -23,7 +23,7 @@ export default function AuthButtonClient({
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: "https://bluebird-wine.vercel.app:3000/auth/callback",
       },
     });
   };
